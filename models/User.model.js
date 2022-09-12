7760b826453b06e7540e92fa234e0af52102a5ba
Hector <villaescusahector@gmail.com>
2022-09-12 15:23:01 +0200

    CRUD

--- models/User.model.js
+++ models/User.model.js
@@ -1,16 +1,26 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      requiere: true,
+      unique: true,
     },
-    password: String,
+    email: {
+      type: String,
+      requiere: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      min: 5,
+      requiere: true
+    }
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
+
     timestamps: true,
   }
 );
