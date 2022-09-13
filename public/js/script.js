332430b4b8b00ebd5b51766c2d26886a66286d38
juca25 <jserranocucarella@gmail.com>
2022-09-13 10:51:31 +0200

    style home/profile/login

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,13 @@ document.addEventListener(
   },
   false
 );
+
+
+()=>{("#inpt_search").on('focus', function () {
+  $(this).parent('label').addClass('active');
+});
+
+("#inpt_search").on('blur', function () {
+  if($(this).val().length == 0)
+    $(this).parent('label').removeClass('active');
+});}
