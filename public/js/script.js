d0a88dafe4fe0ce1a4e0ed500edc351c5a4b31fb
juca25 <jserranocucarella@gmail.com>
2022-09-15 21:01:58 +0200

    style

--- public/js/script.js
+++ public/js/script.js
@@ -7,11 +7,21 @@ document.addEventListener(
 );
 
 
-// ()=>{("#inpt_search").on('focus', function () {
-//   $(this).parent('label').addClass('active');
-// });
+{("#inpt_search").on('focus', function () {
+  $(this).parent('label').addClass('active');
+});
 
-// ("#inpt_search").on('blur', function () {
-//   if($(this).val().length == 0)
-//     $(this).parent('label').removeClass('active');
-// });}
+("#inpt_search").on('blur', function () {
+  if($(this).val().length == 0)
+    $(this).parent('label').removeClass('active');
+});}
+
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
